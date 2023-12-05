6f4ef3154758e515b6dd6654b30fa9bf3bff7f92
Nikita Baryshnikov <super.kronk24@yandex.ru>
2023-12-06 00:57:30 +0300

    test

--- internal/entity/update.go
+++
@@ -1,8 +0,0 @@
-package entity
-
-type UpdateStatus struct {
-	UserID  int     `json:"user_id" db:"user_id"`
-	Order   string  `json:"order" db:"number"`
-	Status  string  `json:"status"`
-	Accrual float64 `json:"accrual" db:"current"`
-}
